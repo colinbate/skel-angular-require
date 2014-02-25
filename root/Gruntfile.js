a9f2ea6ea8597038ab9678bbbd7e1d3d86daa797
Colin Bate <1060912+colinbate@users.noreply.github.com>
2014-02-25 19:20:54 -0400

    Get bower and gruntfile and package.json right...

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,233 +1,198 @@
 module.exports = function(grunt){
-    var pkg = require('./package.json'), //package file
-        i, //iterative member
-        jslink = require(__dirname+'/linker/js.json').link,
-        csslink = require(__dirname+"/linker/css.json").link;
+  var pkg = require('./package.json'), //package file
+      i;
 
-    csslink.push("src/css/**/*.css");
-
-    grunt.initConfig({
-        "pkg": grunt.file.readJSON('package.json'),
-        "bump":{
-            "options":{
-                "files":[
-                    "package.json",
-                    "bower.json"
-                ],
-                "updateConfigs": ["pkg"],
-                "commit": true,
-                "commitMessage": "Release v%VERSION%",
-                "commitFiles": ["-a"],
-                "createTag": true,
-                "tagName": "v%VERSION%",
-                "tagMessage": "Version %VERSION%",
-                "push": true,
-                "pushTo": "origin"
+  grunt.initConfig({
+    "pkg": grunt.file.readJSON('package.json'),
+    "bump": {
+      "options": {
+        "files": [
+          "package.json",
+          "bower.json"
+        ],
+        "updateConfigs": ["pkg"],
+        "commit": true,
+        "commitMessage": "Release v%VERSION%",
+        "commitFiles": ["-a"],
+        "createTag": true,
+        "tagName": "v%VERSION%",
+        "tagMessage": "Version %VERSION%",
+        "push": true,
+        "pushTo": "origin"
+      }
+    },
+    "karma": {
+      "unit-pre": {
+        "configFile": "karma.unit.pre.js",
+        "autowatch": false
+      },
+      "unit-post":{
+        "configFile": "karma.unit.post.js",
+        "autowatch": false
+      }
+    },
+    concurrent: {
+      "environment": {
+        "tasks": ["watch","connect:src"],
+        "options": {
+          "logConcurrentOutput": true
+        }
+      }
+    },
+    less: {
+      dev: {
+        options: {
+          dumpLineNumbers: 'mediaquery'
+        },
+        files: {
+          'src/css/core.css': 'src/less/core.less'
+        }
+      },
+      clean: {
+        files: {
+          'src/css/core.css': 'src/less/core.less'
+        }
+      }
+    },
+    jshint: {
+      options: {
+        bitwise: true,
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        nonew: true,
+        plusplus: true,
+        quotmark: true,
+        sub: true,
+        strict: true,
+        undef: true,
+        unused: true,
+        trailing: true,
+        eqnull: true,
+        browser: true,
+        expr: true,
+        globals: {
+          define: false,
+          require: false,
+          module: false
+        },
+      },
+      all: ["src/js/**/*.js"]
+    },
+    clean: {
+      dist: ["dist"],
+      process: ["process"]
+    },
+    watch:{
+      less: {
+        files: ['src/less/**/*.less'],
+        tasks: ['less:dev']
+      },
+      js: {
+        files: ['src/js/**/*.js'],
+        tasks: ['jshint']
+      }
+    },
+    connect: {
+      src: {
+        options: {
+          port: 8059,
+          base: 'src',
+          keepalive: true
+        }
+      },
+      dist: {
+        options: {
+          port: 8059,
+          base: 'dist',
+          keepalive: true
+        }
+      }
+    },
+    bowercopy: {
+      libs: {
+        options: {
+          destPrefix: 'src/lib'
+        },
+        files: {
+          'require.js':  'requirejs/require.js',
+          'angular.js':  'angular/angular.js',
+          'angular-mocks.js' : 'angular-mocks/angular-mocks.js',
+          'normalize.css':  'normalize-css/normalize.css'
+        }
+      }
+    },
+    requirejs: {
+      compile: {
+        options: {
+          appDir: 'src/',
+          baseUrl: 'js',
+          removeCombined: true,
+          optimizeCss: 'none',
+          mainConfigFile: 'src/js/main.js',
+          dir: 'build',
+          uglify: {
+            max_line_length: 200
+          },
+          modules: [
+            // {
+            //   name: 'angular',
+            //   include: ['ngstorage', 'ngdialog']
+            // },
+            {
+              name: 'main',
+              //exclude: ['angular']
             }
-        },
-        "karma":{
-            "unit-pre":{
-                "configFile": "karma.unit.pre.js",
-                "autowatch": false
-            },
-            "unit-post":{
-                "configFile": "karma.unit.post.js",
-                "autowatch": false
-            }
-        },
-        "express":{
-            "server":{
-                "options":{
-                    "script":"server.js"
-                }
-            }
-        },
-        "concurrent":{
-            "environment":{
-                "tasks":["watch","express"],
-                "options":{
-                    "logConcurrentOutput":true
-                }
-            }
-        },
-        "strip":{
-            "dist":{
-                "src":"process/app.js",
-                "options":{
-                    "inline":true,
-                    "nodes":[
-                        "console.log",
-                        "console.warn",
-                        "debugger"
-                    ]
-                }
-            }
-        },
-        "cssmin":{
-            "options":{
-                "banner": "/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today('dd/mm/yyyy') %> */",
-                "report":"gzip"
-            },
-            "dist":{
-                "files":{
-                    "dist/app.css":csslink
-                }
-            }
-        },
-        "plato":{
-            "report":{
-                "options":{
-                    "jshint":false
-                },
-                "files":{
-                    "analytics/complexity":["src/js/**/*.js"]
-                }
-            }
-        },
-        "jshint":{
-            "options":{
-                "smarttabs":true,
-                "unused":false,
-                "boss":true,
-                "debug":true
-            },
-            "all":["src/js/**/*.js"]
-        },
-        "clean":{
-            "dist":["dist"],
-            "process":["process"]
-        },
-        "watch":{
-            "files":[
-                "README.md",
-                "src/**/*.*",
-                "test/**/*.spec.js"
+          ]
+        }
+      }
+    },
+    // "ngtemplates":{
+    //   "{%= sterileName %}":{
+    //     "cwd": "src/html/",
+    //     "src": ["**/*.html"],
+    //     "dest": "process/templates.js",
+    //     "options":{
+    //       "htmlmin":{
+    //         "collapseWhitespace":true
+    //       }
+    //     }
+    //   }
+    // },
+    "copy":{
+      "maps":{
+        "files":[
+          {
+            "cwd": "src/html/",
+            "expand": true,
+            'src':[
+              'index.html',
+              'partials/**/*.*',
+              'directives/**/*.*'
             ],
-            "tasks":["test"],
-            "options":{
-                "livereload":true,
-                "atBegin":true
-            }
-        },
-        "concat":{
-            "options":{
-                "stripBanners":true,
-                "banner": "/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today('dd/mm/yyyy') %> */",
-                "process":true
-
-            },
-            "app":{
-                "src":[
-                    "src/js/{%= sterileName %}.js",
-                    "src/js/**/!({%= sterileName %}).js"
-                ],
-                "dest":"process/app.js"
-            },
-            "vendor":{
-                "src": jslink,
-                "dest":"dist/vendor.js"
-            }
-        },
-        "ngtemplates":{
-            "{%= sterileName %}":{
-                "cwd": "src/html/",
-                "src": ["**/*.html"],
-                "dest": "process/templates.js",
-                "options":{
-                    "htmlmin":{
-                        "collapseWhitespace":true
-                    }
-                }
-            }
-        },
-        "uglify":{
-            "options":{
-                "mangle":false,
-                "report":"min",
-                "wrap":true,
-                "compress":{
-                    "dead_code":true,
-                    "drop_debugger":true,
-                    "sequences":true,
-                    "properties":true,
-                    "comparisons":true,
-                    "evaluate":true,
-                    "booleans":true,
-                    "loops":true,
-                    "unused":true,
-                    "if_return":true,
-                    "join_vars":true,
-                    "cascade":true,
-                    "warnings":true
-                }
-            },
-            "app":{
-                "options":{
-                    "sourceMap":"dist/app.map",
-                    "sourceMappingURL":"app.map",
-                    "report":"gzip",
-                    "banner": "/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today('yyyy-mm-dd') %> */"
-                },
-                "files":{
-                    "dist/app.js":[
-                        "process/app.js",
-                        "process/templates.js"
-                    ]
-                }
-            }
-        },
-        /*
-        "docular":{
-            "baseUrl":"http://127.0.0.1:9001/docs/",
-            "docAPIOrder":["{%=name%}"],
-            "docular_webapp_target":"docs",
-            "groups":[
-                {
-                    "groupTitle":"{%=name%}",
-                    "groupId":"{%=sterileName%}",
-                    "sections":[
-                        {
-                            "id":"{%=sterileName%}",
-                            "title": "{%=name%}",
-                            "scripts":[
-                                "src/js/"
-                            ],
-                            "docs":[
-                                "README.md"
-                            ]
-                        }
-                    ]
-                }
-            ]
-        },*/
-        "copy":{
-            "maps":{
-                "files":[
-                    {
-                        "expand":true,
-                        "flatten": true,
-                        "src":[
-                            "src/html/index.html",
-                            "vendor/managed/**/*.map"
-                        ],
-                        "dest":"dist/",
-                        "filter":"isFile"
-                    }
-                ]
-            }
-        }
-    });
-
-    for(i in pkg.devDependencies){ //iterate through the development dependencies
-        if(pkg.devDependencies.hasOwnProperty(i)){ //avoid iteration over inherited object members
-            if(i.substr(0,6) == 'grunt-'){ //only load development dependencies that being with "grunt-""
-                grunt.loadNpmTasks(i); //load all grunt tasks
-            }
-        }
+            "dest": "dist/",
+            "filter": "isFile"
+          }
+        ]
+      }
     }
-    grunt.registerTask('default',["concurrent"]);
+  });
 
-    grunt.registerTask('test',[ 'clean:dist','jshint','karma:unit-pre','concat','strip','ngtemplates','uglify:app','cssmin',"copy:maps",                  'plato']);
-    grunt.registerTask('build',['clean:dist','jshint','karma:unit-pre','concat','strip','ngtemplates','uglify:app','cssmin',"copy:maps",'karma:unit-post','plato']);
-    grunt.registerTask('dist',[ 'clean:dist','jshint','karma:unit-pre','concat','strip','ngtemplates','uglify:app','cssmin',"copy:maps",'karma:unit-post','plato','bump']);
+  for(i in pkg.devDependencies) { //iterate through the development dependencies
+      if (pkg.devDependencies.hasOwnProperty(i)) { //avoid iteration over inherited object members
+          if (i.substr(0,6) == 'grunt-') { //only load development dependencies that being with "grunt-""
+              grunt.loadNpmTasks(i); //load all grunt tasks
+          }
+      }
+  }
+  grunt.registerTask('default',["concurrent"]);
+
+  grunt.registerTask('install', ['bowercopy', 'less:clean']);
+
+  grunt.registerTask('test',[ 'clean:dist','jshint','karma:unit-pre','concat','strip','ngtemplates','uglify:app','cssmin',"copy:maps",                  'plato']);
+  grunt.registerTask('build',['clean:dist','jshint','karma:unit-pre','concat','strip','ngtemplates','uglify:app','cssmin',"copy:maps",'karma:unit-post','plato']);
+  grunt.registerTask('dist',[ 'clean:dist','jshint','karma:unit-pre','concat','strip','ngtemplates','uglify:app','cssmin',"copy:maps",'karma:unit-post','plato','bump']);
 };
--- template.js
+++ template.js
@@ -4,29 +4,45 @@ exports.description = "Create an Angular Module, including Karma unit tests";
 exports.notes = "";
 
 exports.after = "You should now install project dependencies with _npm install_," +
-    " followed by _bower install_.  After that, you may start the environment with _grunt_.";
+    " followed by _grunt install_.  After that, you may start the environment with _grunt_.";
 
 exports.warnOn = '*';
 
-exports.template = function(grunt, init, done){
-    init.process({},[
+exports.template = function(grunt, init, done) {
+    init.process({}, [
         init.prompt("name"),
         init.prompt("description"),
         init.prompt("version"),
-        init.prompt("repository"),
-        init.prompt("homepage"),
-        init.prompt("bugs"),
-        init.prompt("author")
-    ],function(err,props){
+        init.prompt("repo", 'bitbucket'),
+        init.prompt("repo_type", 'mercurial'),
+        init.prompt("homepage", 'bitbucket'),
+        init.prompt("bugs", 'bitbucket'),
+        init.prompt("author_name"),
+        init.prompt("author_email")
+    ], function(err, props) {
 
         props.keywords = [];
+
+        if (props.repo === 'bitbucket' && init.defaults.username) {
+            props.repo = 'https://bitbucket.org/' + init.defaults.username + '/' + props.name;
+        }
+        props.repository = {
+            type: props.repo_type,
+            url: props.repo
+        };
+        delete props['repo_type'];
+        delete props['repo'];
+
+        if (props.homepage === 'bitbucket' && init.defaults.username) {
+            props.homepage = 'https://bitbucket.org/' + init.defaults.username + '/' + props.name;
+        }
+        if (props.bugs === 'bitbucket' && init.defaults.username) {
+            props.bugs = 'https://bitbucket.org/' + init.defaults.username + '/' + props.name + '/issues';
+        }
 
         props.sterileName = props.name.replace(/\W/g, '');
 
         props.devDependencies = {
-            "express":"~3.4.0",
-            "grunt-plato": "~0.2.1",
-            "grunt-docular": "latest",
             "grunt-angular-templates":"~0.4.1",
             "grunt-contrib-cssmin":"~0.6.2",
             "grunt-contrib-watch": "~0.5.3",
@@ -35,22 +51,20 @@ exports.template = function(grunt, init, done){
             "grunt-concurrent": "~0.3.1",
             "grunt-karma": "~0.6.2",
             "grunt-contrib-jshint": "~0.6.4",
-            "grunt-strip": "~0.2.1",
-            "grunt-contrib-uglify": "~0.2.4",
             "grunt": "~0.4.1",
-            "grunt-express-server":" ~0.4.3",
             "grunt-contrib-connect": "~0.5.0",
             "karma-coverage": "~0.1.0",
             "grunt-contrib-concat": "~0.3.0",
-            "grunt-bump": "0.0.11"
+            "grunt-bump": "0.0.11",
+            "grunt-bowercopy": "~0.7.1",
+            "grunt-contrib-requirejs": "~0.4.1",
+            "grunt-contrib-less": "~0.9.0"
         };
 
         var files = init.filesToCopy(props);
 
-
-        //init.addLicenseFiles(files, props.licenses);
-        init.copyAndProcess(files,props);
-        init.writePackageJSON('package.json',props);
+        init.copyAndProcess(files, props);
+        init.writePackageJSON('package.json', props);
 
         done();
     });
